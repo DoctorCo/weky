0d476dfc2593a8aae945be6ba8053575551143cc
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-12 20:45:36 +0200

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -4,7 +4,7 @@ const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
     var embed = new Discord.MessageEmbed()
-    .addField(`**Weky Bot Invite**`,`Invite me to the servers that you are by clicking [https://discord.com/oauth2/authorize?client_id=809496186905165834&scope=bot&permissions=2083912822](here)`)
+    .addField(`**Weky Bot Invite**`,`Invite me to the servers that you are by clicking (https://discord.com/oauth2/authorize?client_id=809496186905165834&scope=bot&permissions=2083912822)[here]`)
     message.channel.send(embed)
 }
 
