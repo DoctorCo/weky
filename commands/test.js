814f7583d5334b32d1c79bec6eb5516c77863d89
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-13 09:25:56 +0200

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,5 +1,11 @@
-module.exports.run = async (bot, message, args, client) => {
+const { DiscordAPIError } = require("discord.js");
 
+module.exports.run = async (bot, message, args, client) => {
+    const member = message.mentions.members.first() || message.member;
+    const avatar = (member.user.displayAvatarURL({ format: 'jpg' }));
+let image = await canvacord.gay(avatar)
+let attachment = new DiscordAPIError.MessageAttachment( imabe, "rainbow.png")
+message.channel.send(attachment)
 }
 module.exports.config = {
     name: "burn",
