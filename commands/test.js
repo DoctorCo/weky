1b3bfc9c13fa9afa5d43349ba0327196e69a29e9
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-13 09:28:05 +0200

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,6 +1,7 @@
 const { DiscordAPIError } = require("discord.js");
 
 module.exports.run = async (bot, message, args, client) => {
+    const canvacord = require('canvacord')
     const member = message.mentions.members.first() || message.member;
     const avatar = (member.user.displayAvatarURL({ format: 'jpg' }));
 let image = await canvacord.gay(avatar)
