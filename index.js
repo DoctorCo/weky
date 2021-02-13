26d0cb2d6ff7094d1751168a8ae5cef282b5ce55
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-13 12:58:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,42 @@
-const prefix = require('./botsettings.json');
 const Discord = require('discord.js');
-const { MessageEmbed, Message } = require("discord.js")
-const client = new Discord.Client();
-const fs = require("fs")
-const bot = new Discord.Client();
+const botsettings = require('./botsettings.json');
 
-client.commands = new Discord.Collection();
+const bot = new Discord.Client({disableEveryone: true});
+
+const fs = require("fs");
+bot.commands = new Discord.Collection();
+bot.aliases = new Discord.Collection();
 
 fs.readdir("./commands/", (err, files) => {
+
     if(err) console.log(err)
 
     let jsfile = files.filter(f => f.split(".").pop() === "js") 
     if(jsfile.length <= 0) {
-         console.log("[LOGS] Couldn't Find Commands!");
-         return;
+         return console.log("[LOGS] Couldn't Find Commands!");
     }
-
-    console.log(`Loading commands!`);
 
     jsfile.forEach((f, i) => {
         let pull = require(`./commands/${f}`);
-        console.log(`${i + 1} ${f} loaded!`)
-        client.commands.set(pull.config.name, pull);
-        if(pull.config.aliases) {
-          pull.config.aliases.forEach(alias => {
-            client.commands.set(alias, pull)
-          })
-        }
+        bot.commands.set(pull.config.name, pull);  
+        pull.config.aliases.forEach(alias => {
+            bot.aliases.set(alias, pull.config.name)
+        });
     });
 });
 
+bot.on("message", async message => {
+    if(message.author.bot || message.channel.type === "dm") return;
 
-client.on('ready', async () => {
-    console.log(`Logged in as ${client.user.username} (ID: ${client.user.id} TAG: ${client.user.tag})`)
-    client.user.setActivity('life is hard | /help', { type: 'PLAYING'});
+    let prefix = botsettings.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if(!message.content.startsWith(prefix)) return;
+    let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)))
+    if(commandfile) commandfile.run(bot,message,args)
+
 })
 
-
-
-client.on('message', message => {
-    let messageArray = message.content.toLowerCase().split(" ");
-    let command = messageArray[0];
-    let args = messageArray.slice(1);
-    
-    if(!command.startsWith(prefix)) return;
-    
-    let cmd = client.commands.get(command.slice(prefix.length));
-    if(cmd) cmd.run(client, message, args)});
 bot.login(process.env.token);
