e26477132d49d2ec222edeac27b305b63e80983f
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-13 09:02:32 +0200

    Create test.js

---
+++ commands/test.js
@@ -0,0 +1,12 @@
+const canvas = new Canvacord.burn();
+const member = message.mentions.members.first() || message.member;
+const avatar = await canvas(member.user.displayAvatarURL({ format: 'jpg' }));
+message.channel.send(avatar);
+
+module.exports.config = {
+    name: "burn",
+    description: "a burning image effect command",
+    usage: "/burn",
+    accessableby: "Members",
+    aliases: ["fire"]
+}
