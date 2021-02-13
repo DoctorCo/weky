b2762574234e82927d65432cfd97b9308b6ae71b
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-13 09:12:56 +0200

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,5 +1,5 @@
 module.exports.run = async (bot, message, args, client) => {
-
+let Canvacord = require('canvacord')
 const canvas = new Canvacord.burn();
 const member = message.mentions.members.first() || message.member;
 const avatar = await canvas(member.user.displayAvatarURL({ format: 'jpg' }));
