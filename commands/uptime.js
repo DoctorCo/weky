4de23c46f38a90b9503ab7cc2b3b297db89af459
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-13 18:51:49 +0200

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -5,9 +5,9 @@ let days = Math.floor(client.uptime / 86400000);
       let hours = Math.floor(client.uptime / 3600000) % 24;
       let minutes = Math.floor(client.uptime / 60000) % 60;
       let seconds = Math.floor(client.uptime / 1000) % 60;
+message.channel.send(`__Uptime:__\n${days}d ${hours}h ${minutes}m ${seconds}s`);
 }
-      message.channel.send(`__Uptime:__\n${days}d ${hours}h ${minutes}m ${seconds}s`);
-      module.exports.config = {
+module.exports.config = {
             name: "",
             description: "a trigger image command",
             usage: "/uptime",
