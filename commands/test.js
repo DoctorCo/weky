7a317dc5bd6e4526c5ebffab256e030b18768ced
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-13 09:21:11 +0200

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,15 +1,5 @@
 module.exports.run = async (bot, message, args, client) => {
-let Canvacord = require('canvacord')
-const Canvas = require('canvas')
-let layer = await Canvas.loadImage(Canvacord.assets("IMAGE").FACEPALM);
-let canvas = Canvas.createCanvas(632, 357);
-let ctx = canvas.getContext("2d");
-ctx.fillStyle = "black";
-ctx.fillRect(0, 0, 632, 357);
-let avatar = await Canvas.loadImage(image);
-ctx.drawImage(avatar, 199, 112, 235, 235);
-ctx.drawImage(layer, 0, 0, 632, 357);
-return canvas.toBuffer();
+
 }
 module.exports.config = {
     name: "burn",
