4250037b04c45b0775d81a8ce1281ca04687d472
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-13 16:19:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ fs.readdir("./commands/", (err, files) => {
     });
 });
 
-bot.on("message", async message => {\
+bot.on("message", async message => {
     bot.user.setActivity(`Playing in ${bot.guilds.chache.size} servers | /help`,  {type: "PLAYING"})
 
     if(message.author.bot || message.channel.type === "dm") return;
