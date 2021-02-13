630cce4faf96fe8177d514d688090d8884a4f87f
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-13 17:19:33 +0200

    e

--- commands/help.js
+++ commands/help.js
@@ -9,7 +9,7 @@ module.exports.run = async (bot, message, args) => {
         var embed = new Discord.MessageEmbed()
         .setTitle('Weky Bot Help')
         .setURL('https://www.youtube.com/channel/UCZ_cuJGBis0vi6U3bWmvDIg')
-        .setDescription('😂 Fun:\n `corona`, `rc`, `meme`, `simprate`, `ship`, `noobrate`, `fart`, `cringerate`\n🐱 Animals:\n `cat`, `dog`, `fox`\n🔩 Utility:\n `ping`, `invite`, `suggest`, `report`\n📷 Image:\n`drip`, `gray`, `blur`, `facepalm`, `hitler`, `invert`, `gay`, `trigger`, `rip`, `shit`, `trash`')
+        .setDescription('😂 Fun:\n `corona`, `rc`, `meme`, `simprate`, `ship`, `noobrate`, `fart`, `cringerate`\n🔩 Utility:\n `ping`, `invite`, `suggest`, `report`\n📷 Image:\n`drip`, `gray`, `blur`, `facepalm`, `hitler`, `invert`, `gay`, `trigger`, `rip`, `shit`, `trash`')
         .setColor('#FF2D00')
         .setThumbnail('https://cdn.discordapp.com/emojis/798365870148681729.png?v=1')
         .setTimestamp()
