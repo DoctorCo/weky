332c82dd122998bc93cdcaa8654277d4fbc9336f
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-13 17:10:54 +0200

    Update fox.js

--- commands/fox.js
+++ commands/fox.js
@@ -1,23 +1,18 @@
 const Discord = require('discord.js')
 const got = require('got')
 module.exports.run = async (bot, message, args) => {
-    const embed = new Discord.MessageEmbed()
-    got('https://www.reddit.com/r/fox/random/.json').then(response => {
-        let content = JSON.parse(response.body);
-        let permalink = content[0].data.children[0].data.permalink;
-        let memeUrl = `https://reddit.com${permalink}`;
-        let memeImage = content[0].data.children[0].data.url;
-        let memeTitle = content[0].data.children[0].data.title;
-        let memeUpvotes = content[0].data.children[0].data.ups;
-        let memeDownvotes = content[0].data.children[0].data.downs;
-        let memeNumComments = content[0].data.children[0].data.num_comments;
-        embed.setTitle(`${memeTitle}`)
-        embed.setURL(`${memeUrl}`)
-        embed.setImage(memeImage)
-        embed.setColor('RANDOM')
-        embed.setFooter(`👍 ${memeUpvotes} 👎 ${memeDownvotes} 💬 ${memeNumComments}`)
-        message.channel.send(embed);
-    })
+    const subreddits = [
+        'fox',
+        'foxie',
+        'foxpics',
+        'foxy'
+    ];
+    
+            const data = await fetch(`https://imgur.com/r/${subreddits[Math.floor(Math.random() * subreddits.length)]}/hot.json`)
+                .then(response => response.json())
+                .then(body => body.data);
+            const selected = data[Math.floor(Math.random() * data.length)];
+            return message.channel.send(new MessageEmbed().setImage(`https://imgur.com/${selected.hash}${selected.ext.replace(/\?.*/, '')}`));
 }
 
 module.exports.config = {
