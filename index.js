06cf56a82b77caf6cb9f3087b2abd583e8c745e7
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-14 09:09:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ bot.on("message", async message => {
     //Leaderboard
     if(message.content === "/leaderboard" || message.content === "/lb") {
         const rawLeaderboard = await Levels.fetchLeaderboard(message.guild.id, 5);
-        const leaderboard = await Levels.computeLeaderboard(client, rawLeaderboard);
+        const leaderboard = await Levels.computeLeaderboard(bot, rawLeaderboard);
         if (rawLeaderboard.length < 1) return reply("Nobody's in leaderboard yet.");
 
         const lb = leaderboard.map(e => `${e.position}. ${e.username}#${e.discriminator}\nLevel: ${e.level}\nXP: ${e.xp.toLocaleString()}`);
