7c584e79efc7e912dc19a75f0ecab174cb066c34
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-15 21:35:26 +0200

    Update dep.js

--- commands/dep.js
+++ commands/dep.js
@@ -1,11 +1,7 @@
 const Discord = require('discord.js');
-const { deleteModel } = require('mongoose');
-const deleteAmount = parseInt(args[0]);
 module.exports.run = async (bot, message, args) => {
-    if (!args[0]) return message.reply('You need to type a number')
-    const num = parseFloat(args[0]);
-    if (isNaN(args[0]) || parseInt(args[0]) <= 0) { return message.reply('Please put a number only!')
-        }
+    var num = parseFloat(args[0])
+    if (num.isNaN) return message.channel.send("Thats not a valid number");
     const target = message.mentions.users.first() || message.author
     const targetId = target.id
     const Money = require('../schemas/Money')
@@ -45,13 +41,10 @@ Money.findOne({
     let user = message.guild.members.cache.get(message.author.id);
     user.user.send(`Hello , thanks for starting using Weky Bot!\nYou got 100 coins as reward for starting. Do /help for more commands about our currency system.`)
   } else {
-    if(deleteAmount > data.Cash) {return message.channel.send("You dont have that much money");
-} else {
-       data.Cash -= deleteAmount;
-       data.Bank += deleteAmount;
-       data.save();
-       message.channel.send("Deposited **" + deleteAmount + '** coins')
-  }
+    data.Wallet -= num;
+    data.Bank += num;
+    data.save();
+    message.channel.send(`Successfully deposited **${num} coins**`)
 }
 });
   }
