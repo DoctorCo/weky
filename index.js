e5414dbb437d37966b68112fdea17f169f394ba1
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-15 15:32:46 +0200

    e

--- index.js
+++ index.js
@@ -31,6 +31,25 @@ fs.readdir("./commands/", (err, files) => {
         });
     });
 });
+bot.on('message', (message) => {
+    const ms = require('ms'); // npm install ms
+    const args = message.content.slice(settings.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'start-giveaway') {
+        // g!start-giveaway 2d 1 Awesome prize!
+        // will create a giveaway with a duration of two days, with one winner and the prize will be "Awesome prize!"
+
+        client.giveawaysManager.start(message.channel, {
+            time: ms(args[0]),
+            prize: args.slice(2).join(' '),
+            winnerCount: parseInt(args[1])
+        }).then((gData) => {
+            console.log(gData); // {...} (messageid, end date and more)
+        });
+        // And the giveaway has started!
+    }
+});
 bot.on("message", async message => {
     if(message.author.bot || message.channel.type === "dm") return;
 
