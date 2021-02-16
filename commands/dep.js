aa7a4b686fbd8f9e1c660afa43e33cbb53b3468d
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-16 18:50:07 +0200

    Update dep.js

--- commands/dep.js
+++ commands/dep.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
-    var num = parseFloat(args[0])
+    var num = parseFloat(args[1])
     if (num.isNaN) return message.channel.send("Thats not a valid number");
     const Money = require('../schemas/Money')
 Money.findOne({
@@ -15,8 +15,8 @@ Money.findOne({
     let user = message.guild.members.cache.get(message.author.id);
     user.user.send(`Hello , thanks for starting using Weky Bot!\nYou got 100 coins as reward for starting. Do /help for more commands about our currency system.`)
   } else {
-    data.Wallet -= args[1];
-    data.Bank += args[1]
+    data.Wallet -= num;
+    data.Bank += num
     data.save();
     message.channel.send(`Successfully deposited **${num} coins**`)
 }
