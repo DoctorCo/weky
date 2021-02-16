14e2e4b36885d05bf2082ecb91e7d564a0bc3f31
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-16 17:29:58 +0200

    Update suggest.js

--- commands/suggest.js
+++ commands/suggest.js
@@ -4,10 +4,10 @@ module.exports.run = async (bot, message, args, client) => {
         message.mentions.members.first() ||
         message.guild.members.cache.get('778518819055861761');
 
-      if (!args[0])
+      if (!args[1])
         return message.channel.send("You did not specify your message");
       user.user
-        .send(args[0] + `\n ${message.author.username}#${message.author.discriminator}`)
+        .send(args[1] + `\n ${message.author.username}#${message.author.discriminator}`)
         .then(() => message.channel.send(`Thanks for the suggestion/bug! It will be rated/fixed by Face in some time!`));
     }
 
