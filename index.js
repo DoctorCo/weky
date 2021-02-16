20b2217bfd1ee53cbcba822a6861348e2ae722a8
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-16 18:37:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -51,8 +51,10 @@ bot.on("message", async message => {
 
     let prefix = botsettings.prefix;
     let messageArray = message.content.split(" ");
-    let cmd = messageArray[0];
-    let args = message.content.substring(message.content.indexOf(' ')+1);
+    const input = msg.content.slice(prefix.length).trim().split(' ');
+      const commandArgs = input.join(' ');
+      const args = msg.content.slice(prefix.length).trim().split(/ +/);
+      let cmd = messageArray[0];
 
     if(!message.content.startsWith(prefix)) return;
     let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)))
