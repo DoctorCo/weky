2990ea878f803254abc2db86266030436da5f2d0
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-17 17:08:48 +0200

    Update leaderboard.js

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -12,7 +12,7 @@ module.exports.run = async (bot, message, args) => {
     const leaderboard = await Levels.computeLeaderboard(bot, rawLeaderboard);     
     if (rawLeaderboard.length < 1) return reply("Nobody's in leaderboard yet.");
 
-    const lb = leaderboard.map(e => `**${e.position}. ${e.username}#${e.discriminator}**\n\`Level: ${e.level}\`\nXP: ${e.xp.toLocaleString()}`);
+    const lb = leaderboard.map(e => `${e.position}. ${e.username}#${e.discriminator}\nLevel: ${e.level}\nXP: ${e.xp.toLocaleString()}`);
 
     const background = await Canvas.loadImage('https://cdn.discordapp.com/attachments/795647180995559434/811609649844584478/lb.jpg');
 	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
