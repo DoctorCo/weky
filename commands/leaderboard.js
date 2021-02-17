4df3d5ea3fff5d2b9cf3cb89265d564f14d731dc
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-17 15:49:23 +0200

    Update leaderboard.js

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,16 +1,20 @@
-const Discord = require('discord.js')
-const Levels = require('discord-xp')
-module.exports.run = async (bot, message, args) => {
-const rawLeaderboard = await Levels.fetchLeaderboard(message.guild.id, 5);
-const leaderboard = await Levels.computeLeaderboard(bot, rawLeaderboard);
-if (rawLeaderboard.length < 1) return message.reply("Nobody's in leaderboard yet.");
+const Discord = require('discord.js');
+const Levels = require("discord-xp");
+const Canvas = require('canvas');
 
-const lb = leaderboard.map(e => `${e.position}. **${e.username}#${e.discriminator}**\n\`Level: ${e.level}\`\nXP: ${e.xp.toLocaleString()}`);
-const embed = new Discord.MessageEmbed()
-.setAuthor(`Weky's Leaderboard for "${message.guild.name}"`)
-.setDescription(`${lb.join("\n\n")}`)
-.setFooter(`If the users are Unknown, that means they havent been sent messages anymore`)
-message.channel.send(embed)
+
+module.exports.run = async (bot, message, args) => {
+    Canvas.registerFont('Manrope-Bold.otf', { family:  'imgay' });
+
+        const rawLeaderboard = await Levels.fetchLeaderboard(message.guild.id, 5);
+
+        if (rawLeaderboard.length < 1) return reply("Nobody's in leaderboard.");
+
+        const leaderboard = Levels.computeLeaderboard(bot, rawLeaderboard);
+
+        const lb = leaderboard.map(e => `${e.position} • ${e.username}#${e.discriminator}\nLevel: ${e.level} XP: ${e.xp.toLocaleString()}`);
+
+        //stuff for sending canvas dont worry about this
 }
 module.exports.config = {
     name: "leaderboard",
