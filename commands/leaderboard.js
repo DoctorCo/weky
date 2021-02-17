bdb86115475e713178c3c6f06a5217221faa1ac9
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-17 16:28:22 +0200

    Update leaderboard.js

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -13,9 +13,8 @@ module.exports.run = async (bot, message, args) => {
     // ctx.closePath();
     // ctx.clip();
     const rawLeaderboard = await Levels.fetchLeaderboard(message.guild.id, 5);
+    const leaderboard = await Levels.computeLeaderboard(bot, rawLeaderboard);     
     if (rawLeaderboard.length < 1) return reply("Nobody's in leaderboard yet.");
-
-    const leaderboard = Levels.computeLeaderboard(bot, rawLeaderboard); 
 
     const lb = leaderboard.computeLeaderboard(e => `**${e.position}. ${e.username}#${e.discriminator}**\n\`Level: ${e.level}\`\nXP: ${e.xp.toLocaleString()}`);
 
