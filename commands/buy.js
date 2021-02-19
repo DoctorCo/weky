45d31e086ac98403a21209cca84bd10152844943
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-19 20:17:53 +0200

    Update buy.js

--- commands/buy.js
+++ commands/buy.js
@@ -1,15 +1,12 @@
-
-    const Money = require("../schemas/Money")
+const Money = require("../schemas/Money")
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    //We have to set a argument for the help command beacuse its going to have a seperate argument.
-    let helpArray = message.content.split(" ");
-    let helpArgs = helpArray.slice(1);
+    let buyArray = message.content.split(" ");
+    let buyArgs = buyArray.slice(1);
 
-    //Custom Help command by using the second argument.
-    if(helpArgs[0] === 'laptop') {
+    if(buyArgs[0] === 'laptop') {
         var num = parseFloat(args[2])
     if (num.isNaN) return 
         Money.findOne({
@@ -25,27 +22,39 @@ module.exports.run = async (bot, message, args) => {
             let user = message.guild.members.cache.get(message.author.id);
             user.user.send(`Hello , **thanks for starting using Weky Bot**!\n You got 100 coins as reward for starting. Do \`/help\` for more commands about our currency system.`)
             } else {
-                if(4000*num > data.Wallet) {return message.channel.send(`You dont have money to buy ${num} laptops, make sure that you have the money in wallet`);} else {
-                data.Wallet -= 4000*num;
-                data.Laptop += num;
-                data.save();
-                const embed = new Discord.MessageEmbed()
-                .setAuthor(message.author.username+`#`+message.author.descriminator, message.member.user.displayAvatarURL())
-                .setDescription(`You sucessfully bought 1 Laptop`)
-                message.channel.send(embed)
-        }
+                if(!num){
+                    if(4000 > data.Wallet) {return message.channel.send(`You dont have money to buy ${num} laptops, make sure that you have the money in wallet`);} else {
+                        data.Wallet -= 4000;
+                        data.Laptop += num;
+                        data.save();
+                        const embed = new Discord.MessageEmbed()
+                        .setAuthor(message.author.username+`#`+message.author.descriminator, message.member.user.displayAvatarURL())
+                        .setDescription(`You sucessfully bought **1 Laptop**`)
+                        message.channel.send(embed)
+                    }
+                } else {
+                    if(4000*num > data.Wallet) {return message.channel.send(`You dont have money to buy ${num} laptops, make sure that you have the money in wallet`);} else {
+                        data.Wallet -= 4000*num;
+                        data.Laptop += num;
+                        data.save();
+                        const embed = new Discord.MessageEmbed()
+                        .setAuthor(message.author.username+`#`+message.author.descriminator, message.member.user.displayAvatarURL())
+                        .setDescription(`You sucessfully bought **${num} Laptop**`)
+                        message.channel.send(embed)
+                }
+            }
+                
+
                
             }
         })
     }
 
-    //Normal usage of (prefix)help without any args. (Shows all of the commands and you should set the commands yourself)
-    if(!helpArgs[0]) {
+    if(!buyArgs[0]) {
         
         message.channel.send(`Ye ye weky here what you want to buy, /shop for the list :rolling_eyes:`);
     }
 
-    //Reads the moudle.exports.config (This line of code is on commands folder, each command will read automaticly) by the second argument (the command name) and shows the information of it.
 
 }
 
